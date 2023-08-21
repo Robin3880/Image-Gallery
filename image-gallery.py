f02827afe9c3e11d0d25a13f40232f285c40c20a
Robin3880 <robinkonrad101@gmail.com>
2023-08-21 11:35:15 +0100

    formatted code and added comments

--- image-gallery.py
+++ image-gallery.py
@@ -2,7 +2,7 @@ import customtkinter
 from PIL import Image
 import os
 
-class ToplevelWindow(customtkinter.CTkToplevel):
+class ToplevelWindow(customtkinter.CTkToplevel):             #this is the image window opened when an image is pressed
     def __init__(self, *args, img, **kwargs):
         super().__init__(*args, **kwargs)
         
@@ -10,12 +10,13 @@ class ToplevelWindow(customtkinter.CTkToplevel):
         with Image.open(f"Images/{img}") as imeg:
             width, height = imeg.size
 
+        # Create a custom image object
         image = customtkinter.CTkImage(dark_image=Image.open(f"Images/{img}"), size=(width, height))
         self.label = customtkinter.CTkLabel(self, text="", image=image)
         self.label.pack(padx=0, pady=0)
         self.geometry(f"{width}x{height}")
 
-class App(customtkinter.CTk):
+class App(customtkinter.CTk):                                #this is the image gallery window
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.title("Image-Gallery")
@@ -25,11 +26,12 @@ class App(customtkinter.CTk):
             
         ImagesFolder = []
 
+        # Get the list of image files in the "Images" folder
         for filename in os.listdir("Images"):
             ImagesFolder.append(os.path.join("Images", filename))
 
-        images = {}
         for num, img1 in enumerate(ImagesFolder):
+            # Create a custom image object for each image file
             image = customtkinter.CTkImage(dark_image=Image.open(f"{img1}"), size=(80, 80))
             start, img = img1.split("\\")
             label = customtkinter.CTkLabel(self, text=f"{img}", image=image, compound="top", fg_color="transparent")
@@ -37,20 +39,18 @@ class App(customtkinter.CTk):
             column = num % 9
             label.grid(row=row, column=column, pady=20, padx=20)                                
             label.bind("<Button-1>", lambda event, img=img: self.open_toplevel(img))
-            label.configure(cursor="hand2")
-            images[img] = (image, label, img1)
-
-                
+            label.configure(cursor="hand2") # Set cursor to hand when hovering over the label
+            
         self.grid_propagate(False)
 
     def open_toplevel(self,img):
         if self.toplevel_window is None or not self.toplevel_window.winfo_exists():
-            self.toplevel_window = ToplevelWindow(self, img=img)
+            self.toplevel_window = ToplevelWindow(self, img=img)  # Create window if it's None or destroyed
             self.toplevel_window.attributes('-topmost', True)
         else:
-            self.toplevel_window.attributes('-topmost', True)
+            self.toplevel_window.attributes('-topmost', True)  # If window exists, focus it
 
-            
+# Create an instance of the App class and run the application
 app = App()
 app.resizable(False, False)
 app.mainloop()
